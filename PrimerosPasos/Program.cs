c056b0002bd2c4b593f7f69b6c7c10c24da93926
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2020-11-12 12:03:43 -0500

    Tipos de datos básicos en C#

--- PrimerosPasos/Program.cs
+++ PrimerosPasos/Program.cs
@@ -10,7 +10,44 @@ namespace PrimerosPasos
     {
         static void Main(string[] args)
         {
+            //tipos de datos
+            int a,b,suma; //declarar una variable tipo entero
+            a = 10;
+            b = 20;
+            suma = a + b;
+            Console.WriteLine("La suma de {0} + {1} = {2}", a, b, suma);
 
+            //tipo de dato: número en coma flotante
+            float sueldo, egreso, total;
+            Console.Write("Ingresa el sueldo de trabajador: ");
+            sueldo= float.Parse( Console.ReadLine());
+
+            Console.Write("Ingresa los gastos del trabajador: ");
+            egreso = float.Parse(Console.ReadLine());
+
+            total = sueldo - egreso;
+
+            Console.WriteLine("El líquido a recibir es: {0}", total);
+            
+            //case sensitive
+
+            //tipo de dato: flotante de doble precisión
+            double x1 = 200.5679;
+            Console.WriteLine("Tipo dato double: {0}", x1);
+
+            //Tipo de dato: Cadena de caracteres (string)
+            String nombre;
+            Console.Write("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre +  ", bienvenido al curso de Programación!!!");
+
+            //tipo dato booleano (tipo lógico: verdadero/falso)  (true/false)
+            bool sw;
+            sw = (a+50) > b;
+            Console.WriteLine("El valor del booleano sw: {0}", sw);
+
+
+            Console.ReadKey();
         }
     }
 }
