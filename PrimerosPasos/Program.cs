b9ca791a93c79553763bf2ce121341543691ff50
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2020-11-12 12:37:47 -0500

    Funciones que devuelven valores y funciones que no devuelven valores

--- PrimerosPasos/Program.cs
+++ PrimerosPasos/Program.cs
@@ -46,8 +46,39 @@ namespace PrimerosPasos
             sw = (a+50) > b;
             Console.WriteLine("El valor del booleano sw: {0}", sw);
 
+            //Pilares fundamentales de la programación estructura
+            //ejecución de sentencias secuenciales
+            //uso estructuras repetitivas
+            //uso estructuras selectivas
+            saludos(nombre);
+            saludos("José Alcívar");
+            saludos("María Castro");
+
+            Console.WriteLine();
+            Console.WriteLine("Llamada a función que devuelve valor");
+            Console.WriteLine("Líquido a recibir para Juan {0}", calcularSueldo(300, 200));
+            Console.WriteLine("Líquido a recibir para Carlos {0}", calcularSueldo(2000, 1000));
+            Console.WriteLine("Líquido a recibir para Lesli {0}", calcularSueldo(1340, 199));
 
             Console.ReadKey();
+        }  //fin del main
+    
+    //funciones: subprocesos - subprogramas - subrutinas
+    // dividir el código en módulos
+    // reutilizar el código
+    // tipo: devuelven valores - no devuelve valores
+    static void saludos(String nombre) //parámetro - argumento
+    {
+        Console.WriteLine("Función que no devuelve valores...");
+        Console.WriteLine("HOLA {0} " , nombre);
+    }
+
+        //función que devuelve valor
+        //la función  devuelve un tipo de dato
+        static int calcularSueldo(int ingreso, int egreso)
+        {
+            return (ingreso-egreso);
         }
+
     }
 }
